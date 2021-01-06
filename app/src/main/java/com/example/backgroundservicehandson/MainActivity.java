ed0943a3f0ad83610b695492ca170c6944c0ecc1
Wadah Mac Kraken <wadah-wleed@hotmail.com>
2021-01-06 17:56:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/backgroundservicehandson/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.backgroundservicehandson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void StartBackgroundService(View view) {
+
+    }
+
+    public void StopBackgroundService(View view) {
+
+    }
+}
