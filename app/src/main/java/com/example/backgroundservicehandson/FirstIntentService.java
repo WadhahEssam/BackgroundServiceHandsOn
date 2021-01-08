06f7cead3f09d3db958023baf1023402b58ba934
Wadah Mac Kraken <wadah-wleed@hotmail.com>
2021-01-08 11:23:18 +0300

    Data exchange between activity and service

--- app/src/main/java/com/example/backgroundservicehandson/FirstIntentService.java
+++ app/src/main/java/com/example/backgroundservicehandson/FirstIntentService.java
@@ -3,6 +3,7 @@ package com.example.backgroundservicehandson;
 import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 public class FirstIntentService extends IntentService {
@@ -28,6 +29,7 @@ public class FirstIntentService extends IntentService {
         // notice that the thread here will not be the ui thread
         Log.i(TAG, "on Handle Intent, Thread " + Thread.currentThread().getName());
 
+        // receiving the data from the intentService, and same way can be used for the startedService.
         int sleepTime = intent.getIntExtra("seconds", 6);
 
         // Here you should perform the task (like uploading a file or playing the music, or other stuff)
@@ -42,6 +44,12 @@ public class FirstIntentService extends IntentService {
             counter++;
         }
 
+
+        // how to broadcast event with the data you want.
+        Intent localIntent = new Intent("first.broadcast");
+        localIntent.putExtra("result", counter);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent); // this will make sure that you are sending the broadcast inside the application
+        // sendBroadcast(localIntent) => this will send the broadcast to every one.
     }
 
     // this method will automatically be called after the work is done
--- app/src/main/java/com/example/backgroundservicehandson/MainActivity.java
+++ app/src/main/java/com/example/backgroundservicehandson/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.backgroundservicehandson;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         resultTextView = findViewById(R.id.text_view_result);
+
     }
 
     public void StartBackgroundService(View view) {
@@ -27,7 +37,31 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, FirstIntentService.class);
 
         // that is the way to push data to the service.
-        intent.putExtra("seconds", 12);
+        intent.putExtra("seconds", 3);
         startService(intent);
+    }
+
+    // register a broadcast receiver
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter("first.broadcast");
+        LocalBroadcastManager.getInstance(this).registerReceiver(firstLocalBroadcastReceiver, intentFilter);
+    }
+
+    // the way to receive broadcasts from the service
+    private BroadcastReceiver firstLocalBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int result = intent.getIntExtra("result", 8);
+            resultTextView.setText(result + "");
+        }
+    };
+
+    // unregister broadcast receiver
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(firstLocalBroadcastReceiver);
     }
 }
